3b5dcd6b309d9994ccd1b4aac57e56078b540151
atroutt <audrey.troutt@gmail.com>
2014-02-12 13:27:44 -0500

    updated app to add second activity

--- gen/com/example/anty/R.java
+++ gen/com/example/anty/R.java
@@ -21,21 +21,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int anty=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080004;
+        public static final int imageView=0x7f080003;
+        public static final int linear=0x7f080001;
+        public static final int scrolly=0x7f080000;
+        public static final int textView=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_details=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int details=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int anty_image_description=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int click_me_button=0x7f050005;
+        public static final int details_paragraph1=0x7f050006;
+        public static final int details_paragraph2=0x7f050007;
+        public static final int details_paragraph3=0x7f050008;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_details=0x7f050004;
     }
     public static final class style {
         /** 
