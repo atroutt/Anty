09ec6bbf3be0623dd9d0c91e9e086c995781385f
atroutt <audrey.troutt@gmail.com>
2014-11-10 18:02:06 -0500

    Remove old version of Aritsan

--- artisan/install.bat
+++
@@ -1,4 +0,0 @@
-@echo off set loggerConfig=%fp%\log4j2.xml
-
-cd build\jar
-java -Dlog4j.configurationFile=%loggerConfig% -jar ArtisanSupport.jar %*
--- src/com/example/anty/DetailsActivity.java
+++ src/com/example/anty/DetailsActivity.java
@@ -1,7 +1,7 @@
 package com.example.anty;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
